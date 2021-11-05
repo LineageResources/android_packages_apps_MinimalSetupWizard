640e33439c13fc650d3d60d4abff2047986513a2
Martin J. Pollard <martin0566@gmail.com>
2021-11-05 12:55:39 -0400

    SUW: Remove classes and resources to create a minimal Setup Wizard
    Brings the Setup Wizard down to the absolute minimum required in order to set up
    a device and leave it in a provisioned state (which isn't always possible if the
    Setup Wizard is simply omitted). Essentially, this leaves only the following:
    
    - Bluetooth setup (which doesn't require user interaction)
    - The final page that shows the logo and start button
    
    All of the other settings from Setup Wizard (i.e. Wi-Fi, SIM, date/time, etc.)
    can already be configured in Settings, so this allows the initial ROM setup to
    be streamlined and let the user "get right into it."
    
    Thanks to XDA user Aqq123, whose work on modifying the Setup Wizard script was
    the inspiration for this patch (which goes much further by actually removing
    all of Setup Wizard's unneeded classes and resources).

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -54,12 +54,7 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.lineageos.setupwizard.BluetoothSetupActivity;
-import org.lineageos.setupwizard.ChooseDataSimActivity;
-import org.lineageos.setupwizard.FingerprintActivity;
-import org.lineageos.setupwizard.MobileDataActivity;
 import org.lineageos.setupwizard.SetupWizardApp;
-import org.lineageos.setupwizard.SimMissingActivity;
-import org.lineageos.setupwizard.WifiSetupActivity;
 import org.lineageos.setupwizard.wizardmanager.WizardManager;
 
 import org.lineageos.internal.util.PackageManagerUtils;
@@ -245,24 +240,6 @@ public class SetupWizardUtils {
     public static void disableComponentsForMissingFeatures(Context context) {
         if (!hasLeanback(context)) {
             disableComponent(context, BluetoothSetupActivity.class);
-        }
-        if (!hasFingerprint(context)) {
-            disableComponent(context, FingerprintActivity.class);
-        }
-        if (!hasTelephony(context)) {
-            disableComponent(context, MobileDataActivity.class);
-            disableComponent(context, SimMissingActivity.class);
-            disableComponent(context, ChooseDataSimActivity.class);
-        }
-        if (!SetupWizardUtils.isMultiSimDevice(context)) {
-            disableComponent(context, ChooseDataSimActivity.class);
-        } else if (simMissing()) {
-            disableComponent(context, MobileDataActivity.class);
-            disableComponent(context, ChooseDataSimActivity.class);
-        }
-        if (!SetupWizardUtils.hasWifi(context) ||
-            isEthernetConnected(context)) {
-            disableComponent(context, WifiSetupActivity.class);
         }
     }
 
